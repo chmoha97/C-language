cb6f46961b33e2dda9359e545cbf15ec5996113e
chmoha97 <chebbamohamed16@gmail.com>
2025-04-20 21:28:34 +0200

    Add files via upload
    The purpose of this code is to compare between three integer number intrduced by the user

---
+++ plus_grand_entier.c
@@ -0,0 +1,35 @@
+//the purpose of this code is to compare between three integer number intrduced by the user
+#include<stdio.h>
+
+//define prototype of the function
+void compare(int a,int b, int c);
+
+
+int main(){
+    //define data
+    int a,b,c;
+    printf("les nombre de a, b et c: ");
+    while (1){
+        scanf("%d",&a);
+        scanf("%d",&b);
+        scanf("%d",&c);
+        //verify the big number between a,b and c and print the result
+        compare(a,b,c);
+    }
+
+}
+
+// The architecture of the function compare
+void compare(int a,int b, int c){
+
+    if(a>b && a>c){
+        printf("a est le plus grand\n");
+    }
+    else if(b>a && b>c){
+        printf("b est le plus grand\n");
+    }
+    else{
+        printf("c est le plus grand\n");
+    }
+
+}
