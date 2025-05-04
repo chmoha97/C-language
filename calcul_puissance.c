5f1efeed4c6c0f2b8f3c0a03a3a686ab0d247b16
MohCH <chebbamohamed16@gmail.com>
2025-05-04 11:58:14 +0200

    Update calcul_puissance.c

--- calcul_puissance.c
+++ calcul_puissance.c
@@ -1,3 +1,4 @@
+//the goal of this code is to calculate the power of number using the function pow()
 #include<stdio.h>
 #include<math.h>
 
@@ -8,14 +9,14 @@ float calcul_puissance(){
     scanf("%f",&a);
     printf("la valeur de composante est : ");
     scanf("%f",&y);
-    p=pow(a,y);
+    p=pow(a,y); //function pow to calculate the power of number
     return p;
 }
 
 int main(){
     float a=calcul_puissance();
 
-    printf("la puissance est : %.2f",a);
+    printf("la puissance est : %.2f",a); // display two numbers after comma
 
 
 
